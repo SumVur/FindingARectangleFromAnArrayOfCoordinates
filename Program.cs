fbd697f299853e6fd8dc090ed3452e7c55aa4967
AlexMukharovskiy <a.mukharovskiy@gmail.com>
2021-04-28 10:58:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindingARectangleFromCoordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
